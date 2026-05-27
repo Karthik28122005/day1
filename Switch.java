ca2028dde0e69eaf153a06bcddb42e07465b845b
Karthik28122005 <karthik17092005@gmail.com>
2026-05-27 10:43:17 +0530

    Add files via upload

---
+++ Switch.java
@@ -0,0 +1,28 @@
+public class Switch {
+    public static void main(String[] args) {
+        int day =6;
+        switch(day){
+            case 1:
+                System.out.println("Sunday");
+                break;
+            case 2:
+                System.out.println("Monday");
+                break;
+            case 3:
+                System.out.println("Tuesday");
+                break;
+            case 4:
+                System.out.println(" Wendesday");
+                break;
+            case 5:
+                System.out.println("Thursday");
+                break;
+            case 6:
+                System.out.println("Firday");
+                break;
+            default:
+                System.out.println("not a valid day");
+        }
+    }
+    
+}
